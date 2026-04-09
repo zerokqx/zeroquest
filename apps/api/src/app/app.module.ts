6f8a4dcc97c817e5af4a4882a5aa8038461afc0d
zerokqx <swiftkey382@gmail.com>
2026-04-10 00:19:51 +0300

    env example

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from '../auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env.local', '.env'],
+      envFilePath: ['.env.local', '.env','.env.example'],
       load: [configuration],
     }),
     AuthModule,
